9f74d031a4e31cf4f4d46365d3ae0828fe360c14
dincer velioglu <dincer.velioglu@gmail.com>
2022-08-02 16:48:16 +0300

    All errors are SOLVED

--- components/sidebaritems.js
+++ components/sidebaritems.js
@@ -4,18 +4,22 @@ import { BsFillArrowDownCircleFill } from 'react-icons/bs'
 
 function SidebarItems({ item }) {
   const [active, setActive] = useState(false)
+  const handleClick = () => {
+    setActive(!active)
+  }
+  // const handleActive = onClick={() => setActive(!active)};
 
   if (item.nodeChild) {
     return (
       <div className={active ? 'sidebar-item active' : 'sidebar-item'}>
         <div className="sidebar-title">
           <span>{item.nodeText}</span>
-          <div className="down-arrow" onClick={() => setActive(!active)}>
-            <BsFillArrowDownCircleFill />
+          <div className="down-arrow">
+            <BsFillArrowDownCircleFill onClick={handleClick} />
           </div>
         </div>
         <div className="sidebar-content">
-          {item.nodeChild.map((child, index) => (
+          {item.nodeChild.map((child) => (
             <SidebarItems key={item.nodeChild.id} item={child} />
           ))}
         </div>

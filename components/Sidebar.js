2ad8aa1e362c7e143e2947014f6d30d09399d32c
dincer velioglu <dincer.velioglu@gmail.com>
2022-08-04 10:09:55 +0300

    next build done

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Items from '../pages/data'
+import Items from '../constants/mocks/nav'
 import SidebarItems from './sidebaritems'
 
 // import boxes from '../pages/data'
@@ -10,9 +10,13 @@ function Sidebar() {
 
   return (
     <div className="sidebar">
-      {Items.map((item, index) => (
-        <SidebarItems key={index} item={item} />
-      ))}
+      {Items.map(
+        (
+          item // it was (item, index)
+        ) => (
+          <SidebarItems key={item.id} item={item} /> // in key it was {index} bec. of ERROR: Do not use array index in key
+        )
+      )}
     </div>
   )
 }

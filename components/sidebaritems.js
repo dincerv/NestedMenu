e484c7535bfc5ae7e6eb0fe386aa56f3c203ec94
dincer velioglu <dincer.velioglu@gmail.com>
2022-08-24 16:25:35 +0300

    child fixed

--- components/sidebaritems.js
+++ components/sidebaritems.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { BsFillArrowDownCircleFill } from 'react-icons/bs'
+
+// import { Button } from './button/Button'
 // import boxes from '../pages/data'
 
 function SidebarItems({ item }) {
   const [active, setActive] = useState(false)
   const handleClick = () => {
+    // console.log(`setting ${!active} for ${JSON.stringify(item)}`)
     setActive(!active)
   }
   // const handleActive = onClick={() => setActive(!active)};
@@ -13,16 +16,20 @@ function SidebarItems({ item }) {
     return (
       <div className={active ? 'sidebar-item active' : 'sidebar-item'}>
         <div className="sidebar-title">
+          {/* <Button onClick={handleClick} className="sidebar-title"> */}
           <span>{item.nodeText}</span>
           <div className="down-arrow">
             <BsFillArrowDownCircleFill onClick={handleClick} />
           </div>
+          {/* </Button> */}
         </div>
-        <div className="sidebar-content">
-          {item.nodeChild.map((child) => (
-            <SidebarItems key={item.nodeChild.id} item={child} />
-          ))}
-        </div>
+        {active && (
+          <div className="sidebar-content">
+            {item.nodeChild.map((child) => (
+              <SidebarItems key={child.nodeId} item={child} />
+            ))}
+          </div>
+        )}
       </div>
     )
   }
